a58654a34185819ebcd54327741db0e749c7857c
dominhdang148 <myclone235@gmail.com>
2024-12-07 20:54:05 +0700

    init project

---
+++ novelhub-be/src/main/java/com/dominhdang/novelhub_be/NovelhubBeApplication.java
@@ -0,0 +1,13 @@
+package com.dominhdang.novelhub_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelhubBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelhubBeApplication.class, args);
+	}
+
+}
---
+++ novelhub-be/src/test/java/com/dominhdang/novelhub_be/NovelhubBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominhdang.novelhub_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelhubBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
